3038c88754da782c0e0f92761417f4c66702937a
Tolga Aksoy <tolga.aksoy.74@gmail.com>
2024-04-24 12:58:06 +0300

    RxJava Added, CompositeDisposable Added in MainActivity,

--- app/src/main/java/com/tolga/trendyolapi/service/TrendyolApi.kt
+++ app/src/main/java/com/tolga/trendyolapi/service/TrendyolApi.kt
@@ -3,6 +3,8 @@ package com.tolga.trendyolapi.service
 import com.tolga.trendyolapi.model.Brand
 import com.tolga.trendyolapi.model.TrendyolBrandsModel
 import com.tolga.trendyolapi.model.TrendyolCategoriesModel
+import io.reactivex.Observable
+import io.reactivex.Observer
 
 import retrofit2.Call
 import retrofit2.http.GET
@@ -13,7 +15,9 @@ interface TrendyolApi {
     //https://api.trendyol.com/sapigw/brands
 
     @GET("sapigw/brands")
-    fun getBrandsData():Call<TrendyolBrandsModel>
+    fun getBrandsData(): Observable<TrendyolBrandsModel>
+    //fun getBrandsData():Call<TrendyolBrandsModel>
     @GET("sapigw/product-categories")
+    //fun getCategiesData(): Observable<TrendyolCategoriesModel>
     fun getCategoriesData(): Call<TrendyolCategoriesModel>
 }
--- app/src/main/java/com/tolga/trendyolapi/view/MainActivity.kt
+++ app/src/main/java/com/tolga/trendyolapi/view/MainActivity.kt
@@ -14,10 +14,14 @@ import com.tolga.trendyolapi.databinding.ActivityMainBinding
 import com.tolga.trendyolapi.model.Brand
 import com.tolga.trendyolapi.model.TrendyolBrandsModel
 import com.tolga.trendyolapi.service.TrendyolApi
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity(),RecyclerViewAdapter.Listener {
@@ -25,11 +29,15 @@ class MainActivity : AppCompatActivity(),RecyclerViewAdapter.Listener {
     private val BASE_URL = "https://api.trendyol.com/"
    private var trendyolBrandsModels: ArrayList<Brand>? = null
     private  var recyclerViewAdapter: RecyclerViewAdapter? = null
+
+    private var compositeDisposable: CompositeDisposable? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        compositeDisposable = CompositeDisposable()
 
         val layoutManager : RecyclerView.LayoutManager = LinearLayoutManager(this)
         binding.recyclerView.layoutManager =layoutManager
@@ -57,45 +65,37 @@ class MainActivity : AppCompatActivity(),RecyclerViewAdapter.Listener {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        val service = retrofit.create(TrendyolApi::class.java)
-        val call = service.getBrandsData()
-
-        call.enqueue(object : Callback<TrendyolBrandsModel>{
-            override fun onResponse(
-                call: Call<TrendyolBrandsModel>,
-                response: Response<TrendyolBrandsModel>
-            ) {
-                if (response.isSuccessful){
-                    response.body()?.let {
-                        trendyolBrandsModels = ArrayList(it.brands)
-
-                        trendyolBrandsModels?.let {
-                            recyclerViewAdapter = RecyclerViewAdapter(it,this@MainActivity)
-                            binding.recyclerView.adapter= recyclerViewAdapter
-                        }
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build().create(TrendyolApi::class.java)
 
 
+        compositeDisposable?.add(retrofit.getBrandsData()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(this::handleResponse))
 
-                        /*for (trendyolModel : Brand in trendyolModels!!){
-                            println(trendyolModel.id)
-                            println(trendyolModel.name)
 
-
-                        }*/
-                    }
-                }
-            }
-
-            override fun onFailure(call: Call<TrendyolBrandsModel>, t: Throwable) {
-                t.printStackTrace()
-            }
-
-        })
     }
 
-    override fun onItemClick(brand: Brand) {
-        Toast.makeText(this,"Clicked : ${brand.name}", Toast.LENGTH_LONG ).show()
+    private fun handleResponse(trendyolBrandsModel: TrendyolBrandsModel) {
+
+        trendyolBrandsModels = ArrayList(trendyolBrandsModel.brands)
+
+        trendyolBrandsModels?.let {
+            recyclerViewAdapter = RecyclerViewAdapter(it, this@MainActivity)
+            binding.recyclerView.adapter = recyclerViewAdapter
+
+        }
     }
+
+        override fun onItemClick(brand: Brand) {
+            Toast.makeText(this, "Clicked : ${brand.name}", Toast.LENGTH_LONG).show()
+        }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable?.clear()
+    }
+
+
 }
